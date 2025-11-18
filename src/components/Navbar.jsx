c768c0c73b71fc75b71f107fd55becdf3e28cd8e
saadaijazattari <saadaijaz1234@>
2025-11-18 06:43:20 +0500

    changed icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
+import { BrickWallFire } from 'lucide-react'
 import React from 'react'
 
 
 const Navbar = () => {
   return (
     <div className='flex bg-gray-800 mb-4  items-center justify-center gap-5 py-4 rounded text-white'>
-      <img src="/images/logos_firebase.png  " alt="" className='w-7'/>
+      <BrickWallFire className='w-10 h-10 text-orange-600'/>
       <h1 className='text-xl font-bold'>Firebase Contact App</h1>
     </div>
   )
--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -1,3 +1,4 @@
+import { UserRound } from 'lucide-react'
 import React from 'react'
 
 const NotFoundContact = () => {
@@ -5,7 +6,7 @@ const NotFoundContact = () => {
     <div className='bg-gray-900'>
       <div className='flex flex-col justify-center items-center h-160 gap-4 sm:flex-row'>
         <div>
-        <img src="/images/pro.png" className='w-40' alt="" />
+        <UserRound className='w-24 h-24 text-gray-400 sm:w-32 sm:h-32' />
         </div>
         <h1 className='text-gray-100 text-xl font-bold sm:text-3xl'>Contact Not Found</h1>
       </div>
@@ -15,3 +16,4 @@ const NotFoundContact = () => {
 }
 
 export default NotFoundContact
+
